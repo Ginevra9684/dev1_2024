2139f6a5570c3318333b5c8a6ed493f3c79bcb98
Ginevra9684 <ginevravallebona98@gmail.com>
2024-06-30 13:19:23 +0200

    aggiunta Tipi di Console e readme aggiustato

---
+++ 01-Tipi-di-Dati/Program.cs
@@ -0,0 +1,48 @@
+﻿﻿// esempio di commento single line
+
+/*
+esempio di commento multi linee
+*/
+
+//esempio di dichiarazione di una variabile
+
+int numero;               // dichiaro una variabile di tipo intero
+
+string nome;              // dichiaro una variabile di tipo stringa
+
+bool flag;                // dichiaro una variabile di tipo booleano
+
+// esempio di inizializzazione di variabili
+
+numero = 10;              // inizializzo la variabile numero con il valore 10
+
+nome = "Mario";           // inizializzo la variabile nome con il valore "Mario"
+
+flag = true;              // inizializzo la variabile flag con il valore true
+
+// esempio di dichiarazione e inizializzazione di variabili
+
+int età = 20;             // dichiaro ed inizializzo la variabile età con il valore 20
+
+string cognome = "Rossi"; // dichiaro ed inizializzo la variabile cognome con il valore "Rossi"
+
+bool isStudente = false;  // dichiaro ed inizializzo la variabile isStudente con il valore false
+
+//esempio di dichiarazione ed inizializzazione di una costante
+
+const double PI = 3.14159; // dichiaro ed inizializzo la costante PI con il valore 3.14159
+
+//esempio di dichiarazione ed inizializzazione di una variabile con la parola chiave var
+
+var altezza = 1,.80;       // dichiaro ed inizializzo la variabile altezza con il valore 1.80
+
+// lo svantaggio è che non è possibile dichiarare una variabile var senza inizializzarla
+
+// REGOLA IMPORTANTE: una variabile deve essere inizializzata prima di essere utilizzata
+
+// REGOLA IMPORTANTE: i nomi delle variabili devono essere significativi e autoesplicativi (meglio non nomi abbreviati)
+
+// In C# il nome di una variabile o di una costante può essere scritto in due modi:
+
+Pascal Case                // NumeroIntero
+Camel Case                 // numeroIntero
