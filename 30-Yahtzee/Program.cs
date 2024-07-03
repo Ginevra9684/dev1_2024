5d5d383253c00761389355e508d6a1e09b0aba57
Ginevra9684 <ginevravallebona98@gmail.com>
2024-07-03 15:52:53 +0200

    Yahtzee da completare

---
+++ 30-Yahtzee/Program.cs
@@ -0,0 +1,26 @@
+﻿            // The computer generates a random object
+
+Random random = new Random();
+
+            // We throw 5 dices on a table
+
+Dictionary<string, int> table = new Dictionary<string, int>();
+
+            // The computer uses the random method to generate the random numbers on the dices
+
+Table.Add("1stDice", random.Next(1,7) );
+Table.Add("2ndDice" , random.Next(1,7) );
+Table.Add("3rdDice" , random.Next(1,7) );
+Table.Add("4thDice" , random.Next(1,7) );
+Table.Add("5thDice" , random.Next(1,7));
+
+foreach (KeyValuePair<string, int> gameElement in table)
+{
+    Console.WriteLine($"The {gameElement.Key} shows the number : {gameElement.Value}");     // Key is the object Value is the number
+}
+
+            // The computer asks if we want to reroll one or more dices
+
+
+
+
---
+++ 30-Yahtzee/README.md
@@ -0,0 +1,7 @@
+## ASSIGNMENT
+
+Si tirano 5 dadi 
+
+Lo scopo è avere più facce possibili con inicato lo stesso numeRO
+
+> ad esempio se ho cinque su 2 dadi posso ritirare gli altri 3 dadi un'altra volta
