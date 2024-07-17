ad0ac9f18e1d2e59a5595fc6c139947b144c7168
Ginevra9684 <ginevravallebona98@gmail.com>
2024-07-17 15:37:46 +0200

    json con interfaccia

---
+++ 65-Lettura-csv-Scrittura-json/Program.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+string path = @"test.csv"; // In questo caso il file csv è nella stessa cartella del programma 
+string[] lines = File.ReadAllLines(path); // legge tutte le righe del file
+string[][] prodotti = new string[lines.Length][]; // crea un array di array di stringhe con la lunghezza del numero di righe del file
+for(int i = 0; i < lines.Length; i++)
+{
+    prodotti[i] = lines[i].Split(','); // assegna ad ogni elemento dell'array di array di stringhe il valore della riga corrispondente divisa in un array di stringhe ed utilizza la virgola come separatore
+}
+for(int i = 0; i < prodotti.Length; i++)
+{
+    string path2 = prodotti[i+1][0] + ".json"; // Crea i files utilizzando la chiave del csv come nome
+    File.Create(path2).Close();
+    File.AppendAllText(path2, JsonConvert.SerializeObject(new { nome = prodotti[i][0], prezzo = prodotti[i][1] })); // Scrive la riga nel file
+}
---
+++ 66-Creazione-json-con-Interfaccia/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"test.json"; // in questo caso il file è nella stessa cartella dal programma
+        File.Create(path).Close(); // crea il file
+        File.AppendAllText(path, "[\n");// scrive la riga nel file
+
+        while(true) // ciclo infinito per permettere all'utente di inserire oiù prodotti finchè non decide di smettere di inserire prodotti
+        {
+            Console.WriteLine("Inserisci nome:");
+            string nome = Console.ReadLine()!; // legge il nome
+            string prezzo = Console.ReadLine()!; // legge il prezzo
+
+            // File.AppendAllText(path, JsonConvert.SerializeObject(new { nome, prezzo }) + ",\n"); // scrive la riga nel file
+            // Serializza l'oggetto con identazione
+            string jsonString = JsonConvert.SerializeObject(new { nome, prezzo }, Formatting.Indented);
+            File.AppendAllText(path, jsonString + ",\n"); // scrive la riga nel file
+
+            Console.WriteLine("vuoi inserire un altro prodotto? (s/n)");
+            string risposta = Console.ReadLine()!;
+            if(risposta == "n")
+            {
+                break;
+            }
+        }
+        // togli l'ultima virgola
+        string file = File.ReadAllText(path);
+        file = file.Remove(file.Length - 2, 1); // gli argomenti -2 e 1 indicano rispettivamente la posizione ed il numero di caratteri da rimuovere della stringa
+        File.WriteAllText(path,file);
+        File.AppendAllText(path, "]"); // scrive la riga nel file
+    }
+}
