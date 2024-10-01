addcc1814e324d87d7de5f1f8458b0d220e13001
Ginevra9684 <ginevravallebona98@gmail.com>
2024-10-01 09:58:35 +0200

    paginazione app razor

--- 81-Razor/Pages/Prodotti.cshtml.cs
+++ 81-Razor/Pages/Prodotti.cshtml.cs
@@ -17,17 +17,33 @@ public class ProdottiModel : PageModel
     }
 
     public IEnumerable<Prodotto> Prodotti { get; set; } // IEnumerable è un'interfaccia che definisce un'enumerazione
+    public int numeroPagine { get; set; }   // Aggiunta di una proprietà per il numero di pagine
 
-    public void OnGet(decimal? minPrezzo, decimal? maxPrezzo)     // Aggiunta di argomenti per filtrare i prodotti per prezzo
+    
+
+    
+
+    public void OnGet(decimal? minPrezzo, decimal? maxPrezzo, int? pageIndex)     // Aggiunta di argomenti per filtrare i prodotti per prezzo
     {
         
-        var prodottiTotali = new List<Prodotto>   // Viene creata una lista di prodotti
+        var prodottiTotali = new List<Prodotto>   // Viene creata una lista di tutti i prodotti
         {
-            new Prodotto { Id = 1 , Nome = "Cuffie" , Dettaglio = "fine scorte" , Prezzo = 100 , Immagine = "img/Immagine1.jpg" },     // Vengono aggiunti 3 prodotti nella lista
-            new Prodotto { Id = 2 , Nome = "Polaroid", Dettaglio = "in stock" , Prezzo = 200 , Immagine = "img/Immagine2.jpg" },
-            new Prodotto { Id = 3 , Nome = "PlayStation 4", Dettaglio = "non disponibile" , Prezzo = 300 , Immagine = "img/Immagine3.jpg" }
+            new Prodotto { Id = 1 , Nome = "cuffie" , Dettaglio = "fine scorte" , Prezzo = 100 , Immagine = "img/Immagine1.jpg" },     // Vengono aggiunti 3 prodotti nella lista
+            new Prodotto { Id = 2 , Nome = "polaroid", Dettaglio = "in stock" , Prezzo = 200 , Immagine = "img/Immagine2.jpg" },
+            new Prodotto { Id = 3 , Nome = "playStation 4", Dettaglio = "non disponibile" , Prezzo = 300 , Immagine = "img/Immagine3.jpg" },
+            new Prodotto { Id = 4 , Nome = "caricatore" , Dettaglio = "fine scorte" , Prezzo = 400 , Immagine = "img/Immagine1.jpg" },     
+            new Prodotto { Id = 5 , Nome = "obiettivo", Dettaglio = "in stock" , Prezzo = 500 , Immagine = "img/Immagine2.jpg" },
+            new Prodotto { Id = 6 , Nome = "tavoletta grafica", Dettaglio = "non disponibile" , Prezzo = 600 , Immagine = "img/Immagine3.jpg" },
+            new Prodotto { Id = 7 , Nome = "mouse" , Dettaglio = "fine scorte" , Prezzo = 700 , Immagine = "img/Immagine1.jpg" },     // Vengono aggiunti 3 prodotti nella lista
+            new Prodotto { Id = 8 , Nome = "tastiera", Dettaglio = "in stock" , Prezzo = 800 , Immagine = "img/Immagine2.jpg" },
+            new Prodotto { Id = 9 , Nome = "controller", Dettaglio = "non disponibile" , Prezzo = 900 , Immagine = "img/Immagine3.jpg" },
+            new Prodotto { Id = 10 , Nome = "nintendo switch" , Dettaglio = "fine scorte" , Prezzo = 1000, Immagine = "img/Immagine1.jpg" },     // Vengono aggiunti 3 prodotti nella lista
+            new Prodotto { Id = 11 , Nome = "auricolari", Dettaglio = "in stock" , Prezzo = 1100, Immagine = "img/Immagine2.jpg" },
+            new Prodotto { Id = 12 , Nome = "visore vr", Dettaglio = "non disponibile" , Prezzo = 1200 , Immagine = "img/Immagine3.jpg" },
+            new Prodotto { Id = 13 , Nome = "casse" , Dettaglio = "fine scorte" , Prezzo = 1300 , Immagine = "img/Immagine1.jpg" },     // Vengono aggiunti 3 prodotti nella lista
+            new Prodotto { Id = 14 , Nome = "amplificatore", Dettaglio = "in stock" , Prezzo = 1400 , Immagine = "img/Immagine2.jpg" },
+            new Prodotto { Id = 15 , Nome = "gamepad", Dettaglio = "non disponibile" , Prezzo = 1500 , Immagine = "img/Immagine3.jpg" }
         };
-
 
         var prodottiFiltrati = new List<Prodotto>();
         foreach (var prodotto in prodottiTotali)
@@ -57,5 +73,14 @@ public class ProdottiModel : PageModel
         }
 
         Prodotti = prodottiFiltrati;
+
+        numeroPagine = (int)Math.Ceiling(Prodotti.Count() / 6.0);   // calcola il numero di pagine
+                                                                    // Math.Ceiling arrotonda il numero di pagine all'intero più vicino
+                                                                    // Prodotti.Count() restituisce il numero di prodotti
+                                                                    // 6.0 è il numero di prodotti per pagina
+
+        Prodotti = Prodotti.Skip(((pageIndex ?? 1) - 1) * 6).Take(6);   // esegue la paginazione
+                                                                    // Skip salta i primi ((pageIndex ?? 1) - 1) * 6 prodotti
+                                                                    // Take prende i sucessivi 6 prodotti
     }
 }
